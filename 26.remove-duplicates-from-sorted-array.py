8bcef4310dd2fe468c660bdf6b688986f7597e67
Shi Rao <github@homoantiquum.com>
2024-02-06 01:08:14 -0500

    Finished 26 and 80

--- 26.remove-duplicates-from-sorted-array.py
+++ 26.remove-duplicates-from-sorted-array.py
@@ -10,6 +10,14 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0
+        for j in range(1, len(nums)):
+            if nums[i] != nums[j]:
+                i += 1
+                nums[i] = nums[j]
+        return i + 1
+
+    def removeDuplicates0(self, nums: List[int]) -> int:
         largest = nums[0]
         i = 1
         for j in range(1, len(nums)):
---
+++ 80.remove-duplicates-from-sorted-array-ii.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=80 lang=python3
+#
+# [80] Remove Duplicates from Sorted Array II
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0
+        count = 1
+        for j in range(1, len(nums)):
+            if nums[i] != nums[j]:
+                count = 1
+                i += 1
+                nums[i] = nums[j]
+            elif count < 2:
+                count += 1
+                i += 1
+                nums[i] = nums[j]
+        return i + 1
+
+
+Solution().removeDuplicates([1, 2, 2, 3, 4, 5, 5])
+# @lc code=end
